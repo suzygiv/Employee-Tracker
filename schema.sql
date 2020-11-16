007f2d3b79ab19854763a5b07cc5d50362ab4e42
suzygiv <suzannegivnish@gmail.com>
2020-11-16 18:29:04 -0500

    updated seed.sql file and schema.sql file

---
+++ schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
+CREATE DATABASE employee_trackerDB;
+
+USE employee_trackerDB;
+
+-- DEPARTMENT TABLE ----
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+ 
+);
+-- DEPARTMENT TABLE ----
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT
+);
+-- EMPLOYEE ROLE TABLE ----
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT
+);
+
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
+
---
+++ seed.sql
@@ -0,0 +1,43 @@
+use employee_trackerDB;
+
+-- DEPARTMENT SEEDS -----
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Marketing");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
+
+-- EMPLOYEE ROLE SEEDS -------
+INSERT INTO role (title, salary, department_id)
+VALUE ("Director of Marketing", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Marketing Product Manager", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 190000, 4);
+
+-- EMPLOYEE SEEDS -------
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Celia", "Boone", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Katie", "Lox", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Sarah","Pinckney",null,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Henry", "Kelley", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Owen", "Ewell", 4, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Patrick", "Daley", 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Ben", "Welsh", 2, 7);
